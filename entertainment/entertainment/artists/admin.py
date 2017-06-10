e55e879178a929fe16af89c81642f241cbf0e6f8
naveensingh <naveen@kisanhub.com>
2017-06-10 10:26:02 +0530

    Setting up admin pages

--- entertainment/entertainment/artists/admin.py
+++ entertainment/entertainment/artists/admin.py
@@ -1,6 +1,22 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.contrib import admin
 
-# Register your models here.
+from entertainment.artists.models import Artist
+from entertainment.movies.models import Genre
+
+
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ('name', 'forte')
+    search_fields = ['name', 'forte']
+    ordering = ['name']
+
+
+admin.site.register(Artist, ArtistAdmin)
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ['name']
+    ordering = ['name']
+
+
+admin.site.register(Genre, GenreAdmin)
--- entertainment/entertainment/artists/models/__init__.py
+++ entertainment/entertainment/artists/models/__init__.py
@@ -1,2 +1,1 @@
 from entertainment.artists.models.Artist import Artist
-
