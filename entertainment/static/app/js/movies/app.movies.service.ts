dc6435d667b3dbecfc2ba6bbb05b1651cdd90e49
naveensingh <naveen@kisanhub.com>
2017-06-13 16:54:39 +0530

    Movies service

---
+++ entertainment/static/app/js/movies/app.movies.service.ts
@@ -0,0 +1,37 @@
+import {Injectable} from "@angular/core";
+import {Headers, Http} from "@angular/http";
+import "rxjs/add/operator/map";
+
+@Injectable()
+export class MoviesService {
+    private moviesUrl = "/api/v1.0/app/entertainment/movies";
+
+    constructor(private http: Http) {
+    }
+
+    static extractDataArray(res: Response) {
+        let body = res.json();
+        return body.results || [];
+    }
+
+    static extractDataObject(res: Response) {
+        let body = res.json();
+        return body || {};
+    }
+
+    static handleError(error: error) {
+        console.log(error);
+        return error;
+    }
+
+    getMovies() {
+        return this.http.get(this.moviesUrl, {search: {search: ""}}).map(MoviesService.extractDataArray)
+            .catch(MoviesService.handleError);
+    }
+
+    getMovie(slug: string) {
+        const url = `${this.moviesUrl}/${slug}`;
+        return this.http.get(url)
+            .map(MoviesService.extractDataObject);
+    }
+}
--- entertainment/static/app/js/movies/movies.component.ts
+++ entertainment/static/app/js/movies/movies.component.ts
@@ -8,14 +8,21 @@ import {MoviesService} from "./app.movies.service";
 
 export class MoviesComponent {
     movies: Array<any>;
+    selectedMovie = {};
+
     constructor(private _moviesService: MoviesService) {
     }
 
     getMovies() {
-        this._moviesService.getMovies().subscribe(movies => this.movies=movies)
+        this._moviesService.getMovies().subscribe(movies => this.movies = movies);
+    }
+
+    getMovie(slug: string) {
+        this._moviesService.getMovie(slug).subscribe(movie => this.selectedMovie = movie);
     }
 
     ngOnInit(): void {
         this.getMovies();
+        this.getMovie("1308f2b6-e63d-4547-bf39-895d5c2eadbb"); // testing random movie for get
     }
 }
