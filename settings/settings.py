625355dc6f249c736c465121437bc75017861933
naveensingh <contact@naveensingh.net>
2017-06-16 02:05:58 +0530

    more shit for heroku

--- settings/settings.py
+++ settings/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
