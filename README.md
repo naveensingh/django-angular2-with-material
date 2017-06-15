126505febef11d4fc1bfa9c4a97d58e07904f305
naveensingh <contact@naveensingh.net>
2017-06-16 03:14:03 +0530

    added readme

---
+++ README.md
@@ -0,0 +1,18 @@
+# Django app with Angular 2 and Material
+
+https://djangoproject.com/
+https://angular.io/
+https://material.angular.io/
+
+A simple project to list down movies as cards and as list.
+
+## How to Use
+
+To use this project, follow these steps:
+
+1. Create your working environment.
+2. Install dependencies (`$ pip install requirements.txt`)
+3. Create a user (`$ python manage.py createsuperuser`)
+4. Migrate databases (`$ python manage.py migrate`)
+5. Add dummy data (`$ python manage.py setup_app`)
+6. (`$ python manage.py runserver`)
--- settings/settings.py
+++ settings/settings.py
@@ -121,16 +121,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-STATIC_URL = '/static/'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+# STATIC_URL = '/static/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static")
-# ]
-# STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+STATIC_URL = '/static/'
 
 
 MEDIA_URL = STATIC_URL + "media/"
