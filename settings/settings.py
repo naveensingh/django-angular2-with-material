7b6568455e27fb39228065ab6235cc211d61abd9
naveensingh <contact@naveensingh.net>
2017-06-18 02:18:38 +0530

    static files for local

--- settings/settings.py
+++ settings/settings.py
@@ -122,18 +122,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 # MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'static/media')
 # UPLOAD_ROOT = os.path.join(PROJECT_ROOT, 'static/media/uploads')
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static")
-# ]
-# STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+STATIC_URL = '/static/'
 
 
 # MEDIA_URL = STATIC_URL + "media/"
